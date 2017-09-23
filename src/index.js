799d83e7b14344cd37e01872d186d4293019514b
David Henley <henleydr@gmail.com>
2017-09-23 17:30:00 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<AppContainer>
+  <App />
+</AppContainer>
+, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default; //eslint-disable-line global-require
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  })
+}
